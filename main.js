c6303756f4a92060497c20b38629c9c19470b68b
Mariia Mamontova <masamamontova06@gmail.com>
2024-04-23 00:44:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function generateBalls() {
+  for (var i = 0; i < Math.floor(window.innerWidth/20); i++) {
+    $(".gooey-animations").append(`
+    <div class="ball"></div>
+  `);
+    var colors = ['#28323B','#FFA036'];
+    $(".ball").eq(i).css({"bottom":"0px","left":Math.random()*window.innerWidth-100,"animation-delay":Math.random()*5+"s","transform":"translateY("+Math.random()*10+"px)","background-color":colors[i%2]});
+  }
+}
+generateBalls();
+
+window.addEventListener('resize', function(e) {
+  $(".gooey-animations .ball").remove();
+  generateBalls();
+})
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+
+ScrollReveal().reveal(".order__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+
+
+
