c361b7b57cbeef236f4eda79131a3dd3870a9585
Mariia Mamontova <masamamontova06@gmail.com>
2024-06-03 17:35:06 +0300

    First commit

--- main.js
+++ main.js
@@ -27,5 +27,18 @@ ScrollReveal().reveal(".order__card", {
 });
 
 
+window.addEventListener('scroll', function(){
+  const header = this.document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+function activeMenu(){
+  const toggleMenu = document.querySelector('.menuToggle');
+  const navigation = document.querySelector('.navigation');
+  toggleMenu.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
+
+
 
 
